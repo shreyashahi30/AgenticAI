b32108f3e902e7700061e2807170384153f8494e
Shreya Shahi <105261451+shreyashahi30@users.noreply.github.com>
2026-09-17 23:19:05 +0530

    Update llm_client.py

--- backend/ai/llm_client.py
+++ backend/ai/llm_client.py
@@ -5,7 +5,7 @@ client = Groq(api_key=os.getenv("GROQ_API_KEY"))
 
 def call_llm(prompt: str):
     response = client.chat.completions.create(
-        model="llama-3.1-8b-instant",
+        model="openai/gpt-oss-20b",
         messages=[{"role": "user", "content": prompt}],
         temperature=0.2,
         max_tokens=700,
